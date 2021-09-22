507a00e1d69b61af254b5731af17377b047a9540
Aresiel <love.dittlau@gmail.com>
2021-09-22 22:03:30 +0200

    "changes"

--- derive.js
+++ derive.js
@@ -1,26 +1,41 @@
+const origTableBody = document.getElementById("derivTable").getElementsByTagName("tbody")[0].innerHTML
+let calc = Desmos.GraphingCalculator(document.getElementById("calculator"), {
+    expressionsCollapsed: true,
+    border: false,
+    pointsOfInterest: false
+})
+
 function derive(func, h, x) {
     /* Tråkig version med bestämd funktion
         return (Math.pow(2,x+h)-Math.pow(2,x))/h
     */
     let parser = math.parser()
-    parser.set("x", x)
-    parser.set("h", h)
+    parser.set("x", math.bignumber(x))
+    parser.set("h", math.bignumber(h))
     parser.evaluate("f(x) = " + func)
     return parser.evaluate("(f(x+h)-f(x))/h")
 }
 
 document.getElementById("deriveButton").addEventListener("click", evt => {
+    let precisionInput = document.getElementById("precisionInput")
+    let precision = Math.round(precisionInput.value ? precisionInput.value : precisionInput.placeholder)
+
+    math.config({
+        number: 'BigNumber',
+        precision: precision
+    })
+
     let rightResults = []
     let leftResults = []
 
     let funcInput = document.getElementById("funcInput")
     let xInput = document.getElementById("xInput")
 
-    let func = funcInput.value ? funcInput.value : "2^x"
-    let x = xInput.value ? xInput.value : "3"
+    let func = funcInput.value ? funcInput.value : funcInput.placeholder
+    let x = xInput.value ? xInput.value : xInput.placeholder
 
     let count = 0;
-    for (i = 1; i >= 1e-10; i /= 10) {
+    for (i = 1; i >= +`1e-${precision-10}`; i /= 10) {
         rightResults[count] = [i, derive(func, i, x)]
         leftResults[count] = [i, derive(func, -i, x)]
         count++
@@ -32,12 +47,12 @@ document.getElementById("deriveButton").addEventListener("click", evt => {
     }
 
     let tableBody = document.getElementById("derivTable").getElementsByTagName("tbody")[0]
-    tableBody.innerHTML = `<tr><th>|h|</th><th>f'(x)+</th><th>f'(x)-</th></tr>`
+    tableBody.innerHTML = origTableBody
     tableBody.innerHTML += genTable
 
     let leftDeriv = leftResults[leftResults.length - 1][1]
     let rightDeriv = rightResults[rightResults.length - 1][1]
-    let Deriv = leftDeriv === rightDeriv ? leftDeriv : "Ej deriverbar"
+    let Deriv = leftDeriv.equals(rightDeriv) ? leftDeriv : "Ej deriverbar"
 
     let leftDerivEl = document.getElementById("leftderiv")
     let rightDerivEl = document.getElementById("rightderiv")
@@ -46,4 +61,16 @@ document.getElementById("deriveButton").addEventListener("click", evt => {
     leftDerivEl.innerText = isNaN(Number(Deriv)) ? leftDeriv : "Inte tillämpbar"
     rightDerivEl.innerText = isNaN(Number(Deriv)) ? rightDeriv : "Inte tillämpbar"
     derivEl.innerText = Deriv
+
+    // Desmos
+    calc.setBlank()
+
+    let funcTex = "f(x)=" + math.parse(func).toTex().replaceAll("~","")
+    console.log(funcTex)
+    calc.setExpression({ id: 'function', latex: funcTex , color: Desmos.Colors.BLUE});
+    if(!isNaN(Number(Deriv))){
+        let tangentTex = "y=" + math.parse(rightDeriv + "x").toTex().replaceAll("~","")
+        calc.setExpression({ id: 'tangent', latex: `y=\\left(${Deriv}\\right)\\left(x-${x}\\right)+f\\left(${x}\\right)`, color: Desmos.Colors.ORANGE})
+        calc.setExpression({ id: 'tangentPoint', latex: `\\left(${x},f\\left(${x}\\right)\\right)`, color: Desmos.Colors.RED, pointStyle: Desmos.Styles.CROSS})
+    }
 })
